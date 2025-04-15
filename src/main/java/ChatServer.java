9e50e94e93ce4358ebdb4759352db9811e6f3534
ENNACHAT Redwan <100237071+EnnachatRedwan@users.noreply.github.com>
2025-04-15 20:05:09 +0100

    added missing files

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import handlers.UserChatHandler;
+import lombok.extern.slf4j.Slf4j;
 
-public class ChatServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+public class ChatServer extends Thread {
+
+    List<UserChatHandler> usersChats = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new ChatServer().start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            log.info("Chat server started on port 8080");
+            log.info("Waiting for clients to connect...");
+            while (true) {
+                Socket newClientSocket = serverSocket.accept();
+                String clientId = UUID.randomUUID().toString();
+                UserChatHandler newUserChatHandler = new UserChatHandler(clientId, newClientSocket, usersChats);
+                usersChats.add(newUserChatHandler);
+                log.info("New client connected: {}", clientId);
+                log.info("{} ip address is {}", clientId, newClientSocket.getRemoteSocketAddress());
+                newUserChatHandler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/handlers/UserChatHandler.java
+++ src/main/java/handlers/UserChatHandler.java
@@ -1,4 +1,60 @@
-package models;
+package handlers;
 
-public class Conversation {
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.List;
+
+@Slf4j
+public class UserChatHandler extends Thread {
+
+    private final String id;
+
+    private final Socket socket;
+    private final List<UserChatHandler> activeUsersChats;
+
+    public UserChatHandler(String id, Socket socket, List<UserChatHandler> activeUsersChats) {
+        this.socket = socket;
+        this.id = id;
+        this.activeUsersChats = activeUsersChats;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                InputStream inputStream = socket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String message = bufferedReader.readLine();
+                if (message.trim().equalsIgnoreCase("exit")) {
+                    activeUsersChats.remove(this);
+                    broadcastMessage("User with id : " + this.id + " disconnected");
+                    log.info("User with id : {} disconnected", this.id);
+                    socket.close();
+                    return;
+                }
+                broadcastMessage(message);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private synchronized void broadcastMessage(String message) {
+        this.activeUsersChats.forEach(chatHandler -> {
+            try {
+                chatHandler.sendMeMessage(message);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        log.info("User with id : {} broadcast message : {}", this.id, message);
+    }
+
+    public void sendMeMessage(String message) throws IOException {
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+        printWriter.println(message);
+    }
 }
